617ff7a4577ebadd1758d1734840134fbb882fc5
trobonox <57040351+trobonox@users.noreply.github.com>
2022-05-14 23:32:46 +0200

    Add custom footer with copyright notice

---
+++ components/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20">
+        <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
+            <div class="flex items-end">
+                <span>Copyright © 2021 - 2022 Ultramarine Linux Team</span>
+            </div>
+            <div class="flex items-center space-x-4">
+                <AppLangSwitcher />
+                <AppColorSwitcher />
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales () {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    }
+  }
+}
+</script>
