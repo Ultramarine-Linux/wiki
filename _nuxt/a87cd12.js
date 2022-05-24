79afe2b2b7e7a1f99c113ee87cbd44516adec4fb
trobonox <trobonox@users.noreply.github.com>
2022-05-24 19:39:52 +0000

    deploy: 48efbdf2e6b84a54307b41c9e6880c21db40e0e8

---
+++ _nuxt/a87cd12.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{276:function(e,t,c){"use strict";c.r(t),t.default={search:{placeholder:'Dokumentation durchsuchen (Drücke "/")'},toc:{title:"Auf dieser Seite"},article:{github:"Bearbeite diese Seite auf GitHub",updatedAt:"Letzte Änderung am"}}}}]);
