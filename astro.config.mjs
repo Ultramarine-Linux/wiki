ab12c3c4265191c2421af9339978daf582f8224c
trobonox <57040351+trobonox@users.noreply.github.com>
2023-01-27 23:15:03 +0100

    Add mdx Astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact()
-	],
-	site: `https://astro.build`,
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(), mdx()],
+  site: `https://astro.build`
 });
