e891f61612559b913363b40d578919a400c30653
trobonox <57040351+trobonox@users.noreply.github.com>
2023-01-28 19:46:07 +0100

    Try to fix edit link v2

--- src/consts.ts
+++ src/consts.ts
@@ -18,7 +18,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/ultramarine-linux/wiki-new`;
+export const GITHUB_EDIT_URL = `https://github.com/Ultramarine-Linux/wiki-new/blob/main`;
 
 export const COMMUNITY_INVITE_URL = `https://discord.gg/bUuQasHdrF`;
 
