b2ff78850b41d90232936a1ac83f1c54556e85f6
trobonox <57040351+trobonox@users.noreply.github.com>
2023-01-28 18:51:32 +0100

    Add site url to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wiki.ultramarine-linux.org',
   integrations: [
   // Enable Preact to support Preact JSX components.
   preact(), mdx()],
